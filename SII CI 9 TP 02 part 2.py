cc859079d5c0ac05b50a718e963756f4a25610f1
Kondjoyan <philippe.kondj@gmail.com>
2025-12-12 14:36:25 +0100

    Add files via upload

--- SII CI 9 TP 02 part 2.py
+++ SII CI 9 TP 02 part 2.py
@@ -4,12 +4,17 @@ Created on Fri Dec 12 12:55:03 2025
 
 @author: PKONDJOYAN1
 """
+
+'''
+1 - Etude préliminaire sin(theta) = theta
+'''
+
+
 import numpy as np
 
 N = 90
 nb = 9000
 theta = np.linspace(1,N,nb)
-print(theta)
 theta_rad = [j*np.pi/180 for j in theta]
 
 resultat = [(np.sin(i)-i)/i for i in theta_rad] # calcul des valeurs de l'opération (en rad)
